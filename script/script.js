e94097156741e631ecfe1a219ab735eb0a5dd7eb
fabio-gn <[fabio.giannasi@outlook.com]>
2023-06-07 19:51:27 +0200

    fix a bug that checks only some list elements

--- script/script.js
+++ script/script.js
@@ -1,57 +1,58 @@
 
-//dichiaro funzione per aggiungere tasks alla lista
-const addTask = function(e){
-    //annullo le impostazioni di default del bottone submit
- e.preventDefault();
 
-    // dichiaro variabile per input field
- const inputFieldName = document.getElementById('what-to-do');
-    // dichiaro variabile per ciò che si scrive nell'input field
- let inputFieldContent = inputFieldName.value;
+const addTask = function(e){ //dichiaro funzione per aggiungere tasks alla lista
+       
+    e.preventDefault(); //annullo le impostazioni di default del bottone submit
+
+    const inputField = document.getElementById('what-to-do'); // dichiaro variabile per input field
+    let inputContent = inputField.value; // dichiaro variabile per ciò che si scrive nell'input field
+        
     
- 
-    //dichiaro variabile per la UL
-let tasksSection = document.querySelector('#list-container ul');
-    //creo l'elemento li
-let taskListElement = document.createElement('li');
-    //do attributo a elemento li
-taskListElement.setAttribute('class', 'task');
+        //dichiaro variabile per la UL
+    let tasksSection = document.querySelector('#list-container ul');
+        //creo l'elemento li
+    let taskListElement = document.createElement('li');
+        //do attributo a elemento li
+    taskListElement.setAttribute('class', 'task');
 
-    //scrivo l'HTML contenuto 
- taskListElement.innerHTML = `
- ${inputFieldContent}<button class="delete-button"><i class="fas fa-trash-alt"></i></button> 
- `
-    //attacco il nuovo elemento li a ul
- tasksSection.appendChild(taskListElement);
+        //scrivo l'HTML contenuto 
+    taskListElement.innerHTML = `
+    ${inputContent}<button class="delete-button"><i class="fas fa-trash-alt"></i></button> 
+    `
+        //attacco il nuovo elemento li a ul
+    tasksSection.appendChild(taskListElement);
 
-    //reset dell'input field
- inputFieldName.value = ''
+        //reset dell'input field
+    inputField.value = ''
 
 
- let markAsDone = function() {
-    
-    this.classList.toggle('done')
+    let markAsDone = function() {
+        
+        this.classList.toggle('done')
 
+
+    }
+
+
+    let task = document.querySelectorAll('#list-container li:last-child');
+    console.log(task)
+    task.forEach(el => el.addEventListener('click', markAsDone));
+
+    const deleteItem = function(el){
+        let itemToRemove = el.target;
+        console.log(itemToRemove);
+        itemToRemove.parentElement.parentElement.remove( )
+
+    }
+
+    let delButton = document.querySelectorAll('#list-container button');
+    delButton.forEach(el => el.addEventListener('click', deleteItem));
 
 }
-let task = document.querySelectorAll('#list-container li');
-task.forEach(el => el.addEventListener('click', markAsDone));
-
-const deleteItem = function(el){
-    let itemToRemove = el.target;
-    console.log(itemToRemove);
-    itemToRemove.parentElement.parentElement.remove( )
-
-}
-
-let delButton = document.querySelectorAll('#list-container button');
-delButton.forEach(el => el.addEventListener('click', deleteItem));
-
-}
 
 
 
-const myForm = document.getElementById('list-form');
+const myForm = document.getElementById('form-container');
 myForm.addEventListener('submit', addTask)
 
 
